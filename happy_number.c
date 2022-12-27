53cb901e9c1806952c5e73956e100e800c986dfa
Udaytejaratnakumar <120442411+Udaytejaratnakumar@users.noreply.github.com>
2022-12-27 21:28:05 +0530

    #include<stdio.h> int update(int n){     int x=0;     while(n)     {         x=x+(n%10)*(n%10);         n/=10;     }return x; }
    int main()
    {
        int n;
        scanf("%d",&n);
        while(1){
         n=update(n);
        if(n<9)
        {
            break;
        }}
        if(n==1||n==7)
        {
            printf("True");
        }else{
            printf("False");
        }
    }

---
+++ happy_number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int update(int n){
+    int x=0;
+    while(n)
+    {
+        x=x+(n%10)*(n%10);
+        n/=10;
+    }return x;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(1){
+     n=update(n);
+    if(n<9)
+    {
+        break;
+    }}
+    if(n==1||n==7)
+    {
+        printf("True");
+    }else{
+        printf("False");
+    }
+}
