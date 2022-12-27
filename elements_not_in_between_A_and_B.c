1501f600f0b85a98a36ba838681058018ae0ea41
Udaytejaratnakumar <120442411+Udaytejaratnakumar@users.noreply.github.com>
2022-12-27 11:21:46 +0530

    #include<stdio.h> int main() {     int n,x,y;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&x,&y);     int flag=0;
        for(int i=0;i<n;i++)
        {
            if(a[i]<x || a[i]>y)
            {
                printf("%d ",a[i]);
                flag=1;
            }
        }
        if(flag==0)
        {
            printf("-1");
        }
    }

---
+++ elements_not_in_between_A_and_B.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,x,y;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&x,&y);
+    int flag=0;
+  
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<x || a[i]>y)
+        {
+            printf("%d ",a[i]);
+            flag=1;
+        }
+    }
+    if(flag==0)
+    {
+        printf("-1");
+    }
+}
