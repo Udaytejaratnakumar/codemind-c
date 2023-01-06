2851ff4c6240d3aefa91e552e464a843598968f6
Udaytejaratnakumar <120442411+Udaytejaratnakumar@users.noreply.github.com>
2023-01-06 11:15:43 +0530

    #include<stdio.h> int check(int n) {     int sum=0;     while(n)     {         int r=n%10;         sum=sum+r*r;         n/=10;     }return sum; } int main() {     int n;     scanf("%d",&n);     while(1)     {         n=check(n);         if(n<9)         {             break;         }     }if(n==1||n==7)     {         printf("True");     }else{         printf("False");     }
    }

--- happy_number.c
+++ happy_number.c
@@ -1,25 +1,30 @@
 #include<stdio.h>
+int check(int n)
+{
+    int sum=0;
+    while(n)
+    {
+        int r=n%10;
+        sum=sum+r*r;
+        n/=10;
+    }return sum;
+}
 int main()
 {
-    int n,sum=0;
+    int n;
     scanf("%d",&n);
-    while(1){
-        while(n)
+    while(1)
+    {
+        n=check(n);
+        if(n<9)
         {
-            int r=n%10;
-            sum=sum+r*r;
-            n/=10;
-        }if(sum>9)
-        {
-            n=sum;
-            sum=0;
-        }else{
             break;
         }
-    }if(sum==1||sum==7)
+    }if(n==1||n==7)
     {
         printf("True");
     }else{
         printf("False");
     }
+    
 }
